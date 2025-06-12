12ce33a8a8d4c99fb1f1d3d84dc844bcbb528194
Rosu Liviu <rosuliviu10@gmail.com>
2025-06-12 22:29:16 +0300

    updated double_list section

--- doubly linked lists/double_list.c
+++ doubly linked lists/double_list.c
@@ -1,173 +1,197 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
 #include <malloc.h>
+#include <stdlib.h>
 
+#define CARD_NUMBER_LENGTH 16
+#define EXPIRING_DATE_FORMAT_LENGTH 5 // MM/YY
 
-struct Friends{
-    char* name;
-    char* favActivity;
-    int age;
+struct BankCard
+{
+	char* holder;
+	char card_no[CARD_NUMBER_LENGTH + 1];
+	float balance;
+	char exp_date[EXPIRING_DATE_FORMAT_LENGTH + 1];
+	char* currency;
 };
-typedef struct Friends Friends;
+typedef struct BankCard BankCard;
 
-struct Node{
-    struct Node* next;
-    struct Node* prev;
-    Friends data;
+struct NodeD
+{
+	BankCard data;
+	struct NodeD* next, * prev; // mem address of the next and previous items (nodes) in the chain
 };
-typedef struct Node Node;
 
-struct DoubleLinkedList{
-    struct Node* head;
-    struct Node* tail;
+typedef struct NodeD NodeD;
+
+struct DoubleList
+{
+	NodeD* head, * tail;
 };
-typedef struct DoubleLinkedList DoubleLinkedList;
+typedef struct DoubleList DoubleList;
 
-//parse
-void parse(DoubleLinkedList dlist){
-    Node* temp = dlist.head;
-    printf("Double list head to tail:\n");
-    while(temp != NULL){
-        printf("%s's favorite activity is %s and he is %d years old.\n", temp->data.name, temp->data.favActivity, temp->data.age);
-        temp = temp->next;
-    }
+// insert a node into a double list (at the end)
+DoubleList insert_node(DoubleList list, BankCard bc)
+{
+	NodeD* new_node = malloc(sizeof(NodeD));
+	new_node->data = bc;
+	new_node->next = NULL;
+	new_node->prev = list.tail;
+
+	if (list.head == NULL)
+	{
+		// the list is empty: list.head == list.tail == NULL
+		list.head = new_node;
+		list.tail = new_node;
+	}
+	else
+	{
+		// there is one node at least in double list
+		list.tail->next = new_node;  // the tail becomes the node before the last one (new_node)
+		list.tail = new_node;		 // update the tail of the lista after adding the new_node at the end
+	}
+
+	return list;
 }
 
-//insert beginning
-DoubleLinkedList insertBeginning(DoubleLinkedList dlist, Friends data){
-    Node* newNode = (Node*)malloc(sizeof(Node));
-    newNode->next = NULL;
-    newNode->data = data;
-    newNode->prev = NULL;
+void parse_list(DoubleList list)
+{
+	NodeD* temp = list.head;
+	printf("Double list head-to-tail:\n");
+	while (temp != NULL)
+	{
+		printf("%s %s\n", temp->data.card_no, temp->data.holder);
+		temp = temp->next;
+	}
 
-    if(dlist.head == NULL){
-        dlist.head = dlist.tail = newNode;
-    }
-    else{
-        newNode->next = dlist.head;
-        dlist.head->prev = newNode;
-        dlist.head = newNode;
-    }
-    return dlist;
+	temp = list.tail;
+	printf("\nDouble list tail-to-head;\n");
+	while (temp != NULL)
+	{
+		printf("%s %s\n", temp->data.card_no, temp->data.holder);
+		temp = temp->prev;
+	}
 }
 
-//insert middle
-DoubleLinkedList insertMiddle(DoubleLinkedList dlist, Friends data){
-    Node* midNode = (Node*)malloc(sizeof(Node));
-    midNode->next = NULL;
-    midNode->data = data;
-    midNode->prev = NULL;
+// delete all node having the same holder's name
+DoubleList delete_nodes_holder(DoubleList list, char* holder_name)
+{
 
-    if(dlist.head == NULL){
-        dlist.head = dlist.tail = midNode;
-    }
-    //insert at the end
-    if(dlist.head->next == NULL){
-        dlist.head->next = midNode;
-        midNode->prev = dlist.head;
-        dlist.tail = midNode;
-    }
-    else{
-        int countNodes = 0;
-        Node* temp = dlist.head;
-        while(temp != NULL){
-            countNodes++;
-            temp = temp->next;
-        }
-        temp = dlist.head;
-        int midPos = countNodes / 2;
-        for(int i = 0; i < midPos - 1; i++){
-            temp = temp->next;
-        }
-        midNode->next = temp->next;
-        midNode->prev = temp;
-        
-        if(temp->next != NULL)
-            temp->next->prev = midNode;
-        else{
-            dlist.tail = midNode;
-        }
-        temp->next = midNode;
-    }
-    return dlist;
 }
 
-//insert at end
-DoubleLinkedList insertEnd(DoubleLinkedList dlist, Friends data){
-    Node* newNode = (Node*)malloc(sizeof(Node));
-    newNode->next = NULL;
-    newNode->data = data;
-    newNode->prev = dlist.tail;
+// swap 2 nodes in the double list by changing the prev and next pointers
 
-    if(dlist.head == NULL){
-        dlist.head = dlist.tail = newNode;
-    }
-    else{
-        dlist.tail->next = newNode;
-        dlist.tail = newNode;
-    }
-    return dlist;
+DoubleList delete_node_middle(DoubleList list, char* card_number)
+{
+	if (list.head != NULL)
+	{
+		// there is one node at least in my double list
+		NodeD* temp = list.head;
+
+		while (temp!= NULL && strcmp(temp->data.card_no, card_number) != 0)
+		{
+			// the temp is not the node to be deleted
+			temp = temp->next;
+		}
+
+		if (temp != NULL)
+		{
+			// the node to be deleted has been identified as temp
+			// there is one single node with card_number in the list (unique values for card_number)
+			if (temp == list.head)
+			{
+				// head will be deleted
+				list.head = list.head->next;
+				if (list.head != NULL)
+				{
+					list.head->prev = NULL; // the second node becomes the new head of the double list
+				}
+				else
+				{
+					list.tail = NULL; // double list becomes an empty list
+				}
+			}
+			else
+			{
+				if (temp == list.tail)
+				{
+					list.tail = list.tail->prev; // update of tail with previous node
+					list.tail->next = NULL; // make the previous node as being the new tail of the double list
+				}
+				else
+				{
+					// temp is not neither head or tail
+					(temp->prev)->next = temp->next; // (temp->prev) is the node right before the temp
+					(temp->next)->prev = temp->prev; // (temp->next) is the node right after the temp
+				}
+			}
+
+			// deallocate the node temp
+			free(temp->data.holder); // deallocate holder within the card structure
+			free(temp->data.currency); // deallocate currency within the card structure
+			free(temp); // deallocate the NODE temp
+		}
+	}
+
+	return list;
 }
 
-//delete beginning
-DoubleLinkedList deleteBeginning(DoubleLinkedList dlist){
-    Node* temp = dlist.head;
-    temp = temp->next;
-    temp->next = temp->next->next;
-    temp->prev = NULL;
 
-    return dlist;
-}
+int main()
+{
+	FILE* f = fopen("data.txt", "r");
+	DoubleList d_list;
+	d_list.head = NULL;
+	d_list.tail = NULL;
 
-int main(){
-    FILE* f = fopen("data.txt", "r");
-    char buffer[250];
-    DoubleLinkedList dlist;
-    dlist.head = NULL;
-    dlist.tail = NULL;
+	unsigned char buffer[200];
+	while (fgets(buffer, sizeof(buffer), f))
+	{
+		BankCard card;
+		char seps[] = ",\n";
+		char* token = strtok(buffer, seps); // splitting started from the beginning of buffer array
+		strcpy(card.card_no, token);
 
-    while(fgets(buffer, sizeof(buffer), f)){
-        Friends friend;
-        char seps[] = ",\n";
-        char* token = strtok(buffer, seps);
+		token = strtok(NULL, seps); // continue the splitting of buffer from the current point
+		strcpy(card.exp_date, token);
 
-        friend.name = malloc(strlen(token) + 1);
-        strcpy(friend.name, token);
-        token = strtok(NULL, seps);
+		token = strtok(NULL, seps); // continue the splitting of buffer from the current point
+		card.holder = malloc(strlen(token) + 1);
+		strcpy(card.holder, token);
 
-        friend.favActivity = malloc(strlen(token) + 1);
-        strcpy(friend.favActivity, token);
-        token = strtok(NULL, seps);
+		token = strtok(NULL, seps); // continue the splitting of buffer from the current point
+		card.currency = malloc(strlen(token) + 1);
+		strcpy(card.currency, token);
 
-        friend.age = atoi(token);
+		token = strtok(NULL, seps); // continue the splitting of buffer from the current point
+		card.balance = (float)atof(token);
 
-        dlist = insertEnd(dlist, friend);
-    }
-    parse(dlist);
+		// insert card data into a simple list
+		d_list = insert_node(d_list, card);
+	}
 
-    printf("\nTesting:\n");
-    Friends f2;
-    f2.name = "The lion";
-    f2.favActivity = "snorting coke";
-    f2.age = 0;
+	fclose(f);
 
-    printf("\nList after inserting in the beginning (The lion):\n");
-    dlist = insertBeginning(dlist, f2);
-    parse(dlist);
-    Friends f3;
-    f3.name = "The Wolf";
-    f3.favActivity = "Gooning";
-    f3.age = 1;
+	printf("Duble list after creation:\n");
+	parse_list(d_list);
 
-    printf("\nList after inserting in the middle (The Wolf):\n");
-    dlist = insertMiddle(dlist, f3);
-    parse(dlist);
+	d_list = delete_node_middle(d_list, "6523667711220094");
+	printf("\nDouble list after deletion of one node:\n");
+	parse_list(d_list);
 
-    printf("\nList after deleting first node:\n");
-    dlist = deleteBeginning(dlist);
-    parse(dlist);
+	// deallocate the entire list by calling the one single node deletion
+	// for the card number taken from the head everytime
+	while (d_list.head != NULL)
+	{
+		d_list = delete_node_middle(d_list, d_list.head->data.card_no);
+	}
+	printf("\nDouble list after deallocation:\n");
+	parse_list(d_list);
 
-    fclose(f);
+
+	// insert a node in the middle of the list (define criteria!)
+	// delete a node at the begining and the end of the list
+
+	return 0;
 }
